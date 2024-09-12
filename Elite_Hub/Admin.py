e04ced236fc09bf59955aead74f3f00834342ca8
Alejandro <mariaeugenialopez456@mail.com>
2024-09-12 18:43:35 -0500

    Organizando models.py

--- Elite_Hub/Admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Usuario, Categoria, Publicacion, Comentario, PQRS
-
-admin.site.register(Usuario)
-admin.site.register(Categoria)
-admin.site.register(Publicacion)
-admin.site.register(Comentario)
-admin.site.register(PQRS)
--- Elite_Hub/settings.py
+++ Elite_Hub/settings.py
@@ -38,13 +38,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Elite_Hub',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     'Elite_Hub'
  
 ]
 
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'Elite_Hub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
 
