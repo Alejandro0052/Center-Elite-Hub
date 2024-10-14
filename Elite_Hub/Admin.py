444ec9f6c8c59c6a8d77d3014c6ae575a6c6737c
Alejandro <mariaeugenialopez456@gmail.com>
2024-10-14 10:51:49 -0500

    Organizando modelos

---
+++ Elite_Hub/Admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Usuario,Deporte,Deportista,Nutricionista, Patrocinador, Marca
+
+
+
+
+
+
+admin.site.register(Usuario)
+admin.site.register(Deporte)
+admin.site.register(Deportista)
+admin.site.register(Nutricionista)
+admin.site.register(Patrocinador)
+admin.site.register(Marca)
+
+
+
+   
--- Elite_Hub/settings.py
+++ Elite_Hub/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     'Elite_Hub'
+    'Elite_Hub'
  
 ]
 
@@ -82,6 +82,8 @@ WSGI_APPLICATION = 'Elite_Hub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -92,8 +94,7 @@ DATABASES = {
         'PORT':'5432'
     }
 }
-
-
+ 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
