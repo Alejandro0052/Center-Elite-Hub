b8339a50e5d05c390ce40bceff75b9926e4bc915
Alejandro <mariaeugenialopez456@gmail.com>
2024-10-14 17:02:21 -0500

    Modificacion

--- Elite_Hub/settings.py
+++ Elite_Hub/settings.py
@@ -49,6 +49,67 @@ INSTALLED_APPS = [
  
 ]
 
+INSTALLED_APPS = [
+    'jazzmin',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'Elite_Hub'
+ 
+]
+
+JAZZMIN_SETTINGS = {
+    "site_title": "Elite Hub",
+    "site_header": "Administración",
+    "site_brand": "Elite Hub",
+    "welcome_sign": "Bienvenido a Elite Hub","copyright": "Elite Hub",
+    "topmenu_links": [
+        {"name": "Home", "url": "/", "permissions": ["auth.view_user"]},
+    ],
+    "custom_links": {
+        "app_name": [
+            {"name": "Custom Link", "url": "/custom-url/"},
+        ]
+    },    
+}
+
+JAZZMIN_SETTINGS["show_ui_builder"] = True
+
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": False,
+    "accent": "accent-lime",
+    "navbar": "navbar-dark",
+    "no_navbar_border": False,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": False,
+    "sidebar": "sidebar-dark-lime",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "darkly",
+    "dark_mode_theme": "cyborg",
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    }
+}
 
 
 MIDDLEWARE = [
