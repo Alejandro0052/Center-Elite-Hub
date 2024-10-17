1f04fa08fbc0f2668d6d58ce572950760db15f9f
Alejandro <mariaeugenialopez456@gmail.com>
2024-10-16 23:34:29 -0500

    Tratando de arreglar el modelo

--- Elite_Hub/Admin.py
+++ Elite_Hub/Admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
-from .models import Usuario,Deporte,Deportista,Nutricionista, Patrocinador, Marca
- 
+from Elite_Hub.models import Usuario,Deporte,Deportista,Nutricionista, Patrocinador, Marca, Contenido
  
 class UsuarioAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'apellido', 'correo', 'edad', 'direccion')
+
    
-admin.site.register(Usuario,  UsuarioAdmin)
+
+admin.site.register(Usuario,UsuarioAdmin)
 admin.site.register(Deporte)
 admin.site.register(Deportista)
 admin.site.register(Nutricionista)
 admin.site.register(Patrocinador)
 admin.site.register(Marca)
+admin.site.register(Contenido)
