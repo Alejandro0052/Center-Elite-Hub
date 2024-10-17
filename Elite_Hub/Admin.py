aa41bc782537d1296377596f5ffb7bcb2e57afae
Alejandro <mariaeugenialopez456@gmail.com>
2024-10-16 19:38:26 -0500

    Agregando nueva conexion a base de datos queda pendiente que el sistema muestre las demas clases

--- Elite_Hub/Admin.py
+++ Elite_Hub/Admin.py
@@ -1,25 +1,13 @@
 from django.contrib import admin
 from .models import Usuario,Deporte,Deportista,Nutricionista, Patrocinador, Marca
-
-
+ 
+ 
 class UsuarioAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'apellido', 'correo', 'edad', 'direccion')
-    
-
-
-
-
-
-
-
-
+   
 admin.site.register(Usuario,  UsuarioAdmin)
 admin.site.register(Deporte)
 admin.site.register(Deportista)
 admin.site.register(Nutricionista)
 admin.site.register(Patrocinador)
 admin.site.register(Marca)
-
-
-
-   
