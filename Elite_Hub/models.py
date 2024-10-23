86664793754ad6504747595e004dfdfe11a8ed79
Alejandro0052 <mariaeugenialopez456@gmail.com>
2024-10-22 23:16:59 -0500

    2

--- Elite_Hub/models.py
+++ Elite_Hub/models.py
@@ -70,23 +70,15 @@ class Deportista(models.Model):
 
     def __str__(self):
         return f'{self.usuario}'
->>>>>>> b61cb2f1500eee577051c0e31a95bb311c5e6927
+
     
 class Patrocinador(models.Model):
     usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE, primary_key=True)
     deportistas_interes = models.CharField(max_length=100)
 
-<<<<<<< HEAD
-
     def __str__(self):
         return f'{self.usuario}' 
     
-    
-=======
-    def __str__(self):
-        return f'{self.usuario}' 
-    
->>>>>>> b61cb2f1500eee577051c0e31a95bb311c5e6927
 class Marca(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     razon_social = models.CharField(max_length=60, default='Coloca el nombre de tu empresa')
@@ -94,22 +86,6 @@ class Marca(models.Model):
     def __str__(self):
         return f'{self.usuario}'
 
-<<<<<<< HEAD
-
-#class Invitados(models.Model):
-  # usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
- #   usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE, primary_key=True)
-  #  descripcion = models.CharField(max_length=50)
-   # nombre = models.CharField(max_length=50)
-    #apellido = models.CharField(max_length=50)
-    #numero_telefono = models.IntegerField()
-
-    #def __str__(self):
-     #   return f'{self.nombre} - {self.descripcion}'
-
-
-=======
->>>>>>> b61cb2f1500eee577051c0e31a95bb311c5e6927
 class Pqrs(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     asunto = models.CharField(max_length=30)
@@ -118,53 +94,18 @@ class Pqrs(models.Model):
     def __str__(self):
        return f'{self.usuario} - {self.asunto}' 
 
-<<<<<<< HEAD
-
-class Comentarios(models.Model):
-    usuario = models.ForeignKey(Usuario, models.CASCADE)
-    #marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
-    #nutricionista = models.ForeignKey(Nutricionista, on_delete=models.CASCADE)
-    #deportista = models.ForeignKey(Deportista, on_delete=models.CASCADE)
-    #patrocinador = models.ForeignKey(Patrocinador, on_delete=models.CASCADE)
-    #marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
-    texto = models.TextField()
-    fecha_creacion = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f'{self.usuario}' - {self.texto}
-
-#Agregue related_name='deporte_detail'
-class Deporte(models.Model):
-    deportista = models.OneToOneField(Deportista, related_name='deporte_detail',  on_delete=models.CASCADE, primary_key=True)
-    #nombre = models.CharField(max_length=50)
-    #descripcion = models.TextField()
-=======
 #Agregue related_name='deporte_detail'
 class Deporte(models.Model):
     deportista = models.OneToOneField(Deportista, related_name='deporte_detail',  on_delete=models.CASCADE, primary_key=True)
     descripcion = models.TextField()
->>>>>>> b61cb2f1500eee577051c0e31a95bb311c5e6927
 
     def __str__(self):
        return f'{self.deportista}'
 
-<<<<<<< HEAD
-
-class Contenido(models.Model):
-    usuario = models.ForeignKey(Usuario, models.CASCADE)
-    #deportista = models.ForeignKey(Deportista, on_delete=models.CASCADE)
-   # patrocinador = models.ForeignKey(Patrocinador, on_delete=models.CASCADE)
-    #marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
-    titulo = models.CharField(max_length=55)
-    descripcion = models.TextField()
-    fecha_publicacion = models.DateTimeField(auto_now_add=True)
-    
-=======
 class Contenido(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=55)
     descripcion = models.TextField()
->>>>>>> b61cb2f1500eee577051c0e31a95bb311c5e6927
 
     def __str__(self):
        return f'{self.titulo}'
@@ -181,11 +122,6 @@ class Facturacion(models.Model):
     def __str__(self):
         return f'{self.nombre_usuario}'
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> b61cb2f1500eee577051c0e31a95bb311c5e6927
 class EncabezadoFact(models.Model):
     facturacion = models.OneToOneField(Facturacion, on_delete=models.CASCADE, primary_key=True)
     nombre_usuario = models.CharField(max_length=30)
@@ -194,10 +130,6 @@ class EncabezadoFact(models.Model):
     def __str__(self):
         return f'{self.nombre_usuario}'
 
-<<<<<<< HEAD
-
-=======
->>>>>>> b61cb2f1500eee577051c0e31a95bb311c5e6927
 class DetalleFact(models.Model):
     facturacion = models.OneToOneField(Facturacion, on_delete=models.CASCADE, primary_key=True)
     iva = models.DecimalField(max_digits=5, decimal_places=2)
@@ -207,10 +139,6 @@ class DetalleFact(models.Model):
     def __str__(self):
         return f'{self.nombre_usuario}'
 
-<<<<<<< HEAD
-
-=======
->>>>>>> b61cb2f1500eee577051c0e31a95bb311c5e6927
 class Pagos(models.Model): 
       facturacion = models.OneToOneField(Facturacion, on_delete=models.CASCADE, primary_key=True)
       valor_pago = models.DecimalField(max_digits=10, decimal_places=2)
