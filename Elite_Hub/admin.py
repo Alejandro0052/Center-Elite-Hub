8f7fa3e69f837d15f4f74804607929ed9b2d286d
Alejandro0052 <mariaeugenialopez456@gmail.com>
2024-10-24 23:44:42 -0500

    Commit temporal antes de cambiar de rama

--- Elite_Hub/admin.py
+++ Elite_Hub/admin.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
-from Elite_Hub.models import Usuario,Deporte,Deportista,Nutricionista, Patrocinador, Marca, Contenido, Pqrs
+from Elite_Hub.models import Usuario,Deporte,Deportista,Nutricionista, Patrocinador, Marca, Contenido
  
 class UsuarioAdmin(admin.ModelAdmin):
     list_display = ['nombre', 'apellido', 'numero_telefono', 'correo']
     list_display_links = ['nombre']  # Permitir click en el nombre para ver el detalle
  #   readonly_fields = ['nombre', 'apellido', 'numero_telefono', 'correo',]
+
+
+
+
+
+
+
+
 admin.site.register(Usuario,  UsuarioAdmin)
 admin.site.register(Deporte)
 admin.site.register(Deportista)
 admin.site.register(Nutricionista)
 admin.site.register(Patrocinador)
 admin.site.register(Marca)
-admin.site.register(Pqrs)
 admin.site.register(Contenido)
